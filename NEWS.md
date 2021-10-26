424424a376067f389732bac638c4a960e1af4926
masiraji <siraji1993@gmail.com>
2021-10-26 14:26:04 +0800

    Lifecycle

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# baap 0.0.0.93
+
+* Added a `NEWS.md` file to track changes to the package.
--- data-raw/devtools.R
+++ data-raw/devtools.R
@@ -11,7 +11,7 @@ usethis::browse_github()
 
 
 usethis::use_lifecycle_badge("stable")
-
+usethis::use_lifecycle()
 
 usethis::use_github_action_check_standard()
 usethis::use_cran_badge()
@@ -20,3 +20,29 @@ usethis::use_github_links()
 
 
 usethis::use_build_ignore("index.Rmd")
+
+#Get vaccinated
+usethis::git_vaccinate()
+
+
+usethis::use_spell_check()
+
+#Use magrittr's pipe in your package
+usethis::use_pipe()
+
+
+#Create News
+usethis::use_news_md()
+
+
+
+usethis::use_circleci_badge()
+
+
+usethis::use_github_action_check_standard(
+  save_as = "R-CMD-check.yaml",
+  ignore = TRUE,
+  open = FALSE
+)
+
+
