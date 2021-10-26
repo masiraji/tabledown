64f1fe1149cf181580c1211144ac2e8b005fb6ca
masiraji <siraji1993@gmail.com>
2021-10-26 16:51:10 +0800

    Commit

--- data-raw/devtools.R
+++ data-raw/devtools.R
@@ -1,40 +1,39 @@
 # to create a Rproject
 devtools::create("baap")
 
-
-
 devtools::document()
 devtools::load_all()
 devtools::check()
+usethis::browse_github()
 rm(list = c("ac.tab"))
 
+#packgedown
 pkgdown::build_site(examples = TRUE)
-usethis::browse_github()
+pkgdown::build_home(pkg = ".", override = list(), preview = NA, quiet = TRUE)
 
 
+#Badges
+usethis::use_cran_badge()
+usethis::use_github_links()
+badgecreatr::badge_cran_downloads("baap", period = NULL)
 usethis::use_lifecycle_badge("stable")
 usethis::use_lifecycle()
 
-
-usethis::use_cran_badge()
-usethis::use_github_links()
-usethis::use_github_action_check_standard()
-
+#Usethis
 #Add file to project ignore
 usethis::use_build_ignore(c("data-raw", "docs", "index.Rmd, packages.bib"))
 
 #Get vaccinated
 usethis::git_vaccinate()
 usethis::use_build_ignore(".github")
-
-
+#Spelling
 usethis::use_spell_check()
 
 #Use magrittr's pipe in your package
 usethis::use_pipe()
 
 
-#Create News
+#Create News.md
 usethis::use_news_md()
 #To automate news.md
 devtools::install_github("cynkra/fledge")
@@ -48,8 +47,7 @@ fledge::bump_version()
 
 
 
-#testing the pack
-usethis::use_testthat()
 
-pkgdown::build_home(pkg = ".", override = list(), preview = NA, quiet = TRUE)
+
+
 
