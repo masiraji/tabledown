b70892f11f692a8c8ad02d8e9f028ce85b2d28be
masiraji <siraji1993@gmail.com>
2021-11-23 03:59:03 +0800

    des.tab auto reverse negative items

--- R/des.tab.R
+++ R/des.tab.R
@@ -11,19 +11,20 @@
 #' @export
 des.tab <- function(df){
 
+
   Descriptives <- psych::describe(df)
   Mean <-MOTE::apa(Descriptives$mean,2,T)
   SD <-MOTE::apa(Descriptives$sd,2,T)
   Skew <-MOTE::apa(Descriptives$skew,2,T)
   Kurtosis <- MOTE::apa(Descriptives$kurtosis,2,T)
-  alpha <- psych::alpha(df)
+  alpha <- psych::alpha(df,check.keys=TRUE)
+  Items <- rownames(alpha$item.stats)
   Corrected.item.total.correlation <- MOTE::apa(alpha$item.stats$r.cor,2,T)
 normality.test <-normality.loop(df)
  statistics <-MOTE::apa(normality.test$statistic,2,T)
  sig <-(normality.test$significance)
  Normality <- paste(statistics, sig, sep = "" )
- des.tab <-as.data.frame((cbind(Mean, SD, Skew,Kurtosis,  Normality,Corrected.item.total.correlation))) %>%
-   tibble::rownames_to_column("item")
+ des.tab <-as.data.frame((cbind(Items, Mean, SD, Skew,Kurtosis,  Normality,Corrected.item.total.correlation)))
  des.tab
 
   }
