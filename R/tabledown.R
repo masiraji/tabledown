b4cbe6df7d998cc99d4b7794dd95a2b08b085d11
Mushfiqul Anwar Siraji <siraji1993@gmail.com>
2022-09-26 18:51:27 +0800

    Ds_strore delt with

--- R/tabledown.R
+++ R/tabledown.R
@@ -1,26 +1,28 @@
 #' tabledown: A Companion of the book "Basic and Advanced Psychometrics in R"
 #'
-#' The tabledown package provides necessary data frames used through out the book and some neat functions
+#' The tabledown package provides necessary data frames used throughout the book and some neat functions.
 #'
 #'
 #' @section tabledown Data-frames:
-#' 1. Rotter: Psychometric validation data of Bangla Rotter's Internal- External Data
-#' 2. Gantt and Spot: Two sample data-frames for creating project management Gantt chart
+#' 1. Rotter: Psychometric validation data of Bangla Rotter's Internal- External Scale.
+#' 2. Gantt and Spot: Two sample data-frames for creating project management Gantt chart.
 #' 3. FFMQ.CFA: Structural Validation data of Bangla Five Factor Mindfulness Questionnaire.
 #' 4. FFMQ.Val:Correlational Validity evidences of Bangla Five Factor Mindfulness Questionnaire.
 #'
 #'
 #' @section tabledown functions:
-#'This packages includes some neat functions. Some of the functions presented here are from
-#'stack overflow and other resources that we find very helpful in formatting the outputs
-#'in a publication ready state.
-#'1. fac.tab(): Creates a publication ready table from the output of psych based factor analysis.
+#'This packages includes some neat and useful functions to create tables and figures suitable for journal submission:
+#'1. fac.tab(): Creates a publication ready table from the output of "psych"  package based factor analysis.
 #'2. des.tab(): Creates a publication ready descriptive table of Item analysis with the reporting of normality assumptions.
-#'3. normality.loop(): Compute normality test on the whole data frame. No grouping required.
+#'3. normality.loop(): Compute normality test on the whole data frame. No grouping variable required.
 #'4. bedTime(): Calculate total time spent in bed from the sleep log entry.
-#'5. cfa.tab():Creates a table with necessary fit indices from a lavaan class objects
-#'6. cfa.tab/multi():creates a table with necessary fit indices from several lavaan class objects
-#'
+#'5. cfa.tab():Creates a table with necessary fit indices from a "lavaan" class objects.
+#'6. cfa.tab/multi():creates a table with necessary fit indices from several lavaan class objects.
+#'7. ggicc: Creates a ggplot2 based publication ready Item Characteristics Curve from the "mirt" package based item response theory estimations.
+#'8. ggiteminfo: Creates a ggplot2 based publication ready Item Information Curve from the "mirt" package based item response theory estimations.
+#'9. ggtestinfo: Creates a ggplot2 based publication ready Test Information Curve from the "mirt" package based item response theory estimations.
+#'10. ggtestinfo_se: Creates a ggplot2 based publication ready Test Information Curve with standard error from the "mirt" package based item response theory estimations.
+
 #'It is advisable that you load **tidyverse** along with **tabledown**
 #'
 #' @docType package
