acd0bd42a5627c259393d1d972449a3a22f5cadb
masiraji <siraji1993@gmail.com>
2022-11-18 18:13:29 +0800

    - gt_tab function updated

--- R/gttab.R
+++ R/gttab.R
@@ -1,29 +1,22 @@
-#' A Function for Creating  Publication Quality Factor Tables.
+#' A Function for gtExtra package friendly data summary.
 #'
-#' This function will create publication worthy factor tables from objects created from
+#' This function will gtExtra package friendly data summary using the datafrmae provided
 #' psych pack.
-#' I have came across this beautiful piece of codes
-#' at https://www.anthonyschmidt.co/post/2020-09-27-efa-tables-in-r/ and modified it a bit.
-#'
-#' @param  x A psych package object.
-#' @param  cut The value under which all factor loading will be suppressed.
-#' @param complexity To add complexity parameters.
+
+#' @param  datafrmae Dataframe with all items.
+#' @param  recode_code Recode key
 #'@examples
-#'data <- tabledown::Rotter[, 11:31]
-#'correlations <- psych::polychoric(data, correct = 0)
-#'fa.5F.1 <- psych::fa(r=correlations$rho, nfactors = 5, fm= "pa",rotate ="varimax",
-#'residuals = TRUE, SMC = TRUE, n.obs =428)
-#'table <- fac.tab(fa.5F.1, .3)
-#'#always save the output into an object
+#'data <- tabledown::FFMQ.CFA[, 9:18]
+#'recode_code <- c( "1" = "Never or very rarely true", "2" = "Rarely true", "3"= "Sometimes true","4" = "Often true","5" = "Very often or always true")
+#'sample_tab <- gt_tab(data,recode_code)
 #'@return
-#'A publication ready summary table for the Factor analysis conducted by psych Package. Output structure is data frame.
+#'A publication ready descriptive summary table in png format.
 
 
 #' @importFrom magrittr  %>%
-#'@importFrom dplyr across mutate
+#'@importFrom dplyr
 #'@importFrom tidyselect vars_select_helpers
 #'@importFrom tidyr gather
-#'@importFrom tidyr dplyr group_by, summarise, inner_join, mutate, across, starts_with
 #'@importFrom kutils likert
 #'@importFrom tibble rownames_to_column
 
@@ -39,7 +32,7 @@ gt_tab <- function(dataframe, recode_code){
     dplyr::group_by(Items) %>%
     # calculate summary stats & create data for the histogram and density plot
     dplyr::summarise(
-      nr = n(),
+      nr = dplyr::n(),
       mean = mean(value, na.rm = TRUE),
       med = median(value, na.rm = TRUE),
       sd = sd(value, na.rm = TRUE),
@@ -50,7 +43,7 @@ gt_tab <- function(dataframe, recode_code){
 
 
   descriptive_tab <- tabledown::des.tab(dataframe)
-  summary_tab_2 <- dplyr::inner_join(summary_tab, descriptives_tab, by = "Items")
+  summary_tab_2 <- dplyr::inner_join(summary_tab, descriptive_tab, by = "Items")
   data_likert_1 <- dataframe
   data_likert_2 <-  dplyr::mutate(data_likert_1, dplyr::across(dplyr::starts_with(c("item")), ~unname(recode_code[.])))
   data_Factor_1 = as.data.frame(lapply(data_likert_2,factor, ordered = T))
@@ -72,13 +65,14 @@ gt_tab <- function(dataframe, recode_code){
   col_names <- full_percentage_3[1,]
   full_percentage_4 <-full_percentage_3[-1,]
   full_percentage_5 <- as.data.frame(full_percentage_4)
-  full_percentage_6 <- as.data.frame(full_percentage_5[order(Items),])
-  colnames(full_percentage_6) <- (col_names)
 
-  full.table <-  dplyr::inner_join( summary_tab_2, full_percentage_6, by = "Items")
+  colnames(full_percentage_5) <- (col_names)
+
+  full.table <-  dplyr::inner_join( summary_tab_2, full_percentage_5, by = "Items")
 
   return(full.table )
 }
+
 
 
 
